3456a6f5c8af564cd67e320cb42cbbf0212ed9e1
Will <willwright90@gmail.com>
2017-10-21 12:32:14 +1100

    Simple interface with user info and pic up and running.

--- UNSWtalk.py
+++ UNSWtalk.py
@@ -5,7 +5,7 @@
 # as a starting point for COMP[29]041 assignment 2
 # https://cgi.cse.unsw.edu.au/~cs2041/assignments/UNSWtalk/
 
-import os
+import os, re
 from flask import Flask, render_template, session
 
 students_dir = "dataset-medium";
@@ -16,17 +16,41 @@ app.secret_key = os.urandom(12)
 # Show unformatted details for student "n"
 # Increment n and store it in the session cookie
 
+fields = ['birthday',
+          'courses',
+          'email',
+          'friends',
+          'full_name',
+          'home_latitude',
+          'home_longitude',
+          'home_suburb',
+          'password',
+          'program',
+          'zid']
+
 @app.route('/', methods=['GET','POST'])
 @app.route('/start', methods=['GET','POST'])
 def start():
     n = session.get('n', 0)
+    print(n)
     students = sorted(os.listdir(students_dir))
+    students = [x for x in students if not x.startswith('.')]
     student_to_show = students[n % len(students)]
+    details = {}
     details_filename = os.path.join(students_dir, student_to_show, "student.txt")
+    if os.path.exists(os.path.join(students_dir, student_to_show, "img.jpg")):
+        details['picture'] = os.path.join(students_dir, student_to_show, "img.jpg") 
+    else: details['picture'] = os.path.join("egg.gif")
     with open(details_filename) as f:
-        details = f.read()
+        for line in f:
+            line = line.rstrip()
+            for field in fields:
+                if line.startswith(field):
+                    details[field] = line[len(field)+2:] 
+                    break
+
     session['n'] = n + 1
-    return render_template('start.html', student_details=details)
+    return render_template('start.html', **details) 
 
 if __name__ == '__main__':
     # app.secret_key = os.urandom(12)
