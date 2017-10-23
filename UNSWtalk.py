6fb7f7b95dd2170348059d9c02ba342a4a5fab59
William Wright <w.wright@student.unsw.edu.au>
2017-10-23 11:55:26 +1100

    Update UNSWtalk.py

--- UNSWtalk.py
+++ UNSWtalk.py
@@ -109,7 +109,7 @@ class Comment:
         self.fmessage = self.message
         if re.match('.*z[0-9]{7}.*', self.fmessage):
             for k, v in s.items():
-                self.fmessage = self.fmessage.replace(k,'<a href="/user/'+k+'">'+v.full_name+'</a>')
+                self.fmessage = self.fmessage.replace(k,'<a href=" + url_for('user', zid=k) + '">'+v.full_name+'</a>')
         self.related_to = re.findall('z[0-9]{7}', self.message)
         self.hsh = hash(file)
         self.getReplies() 
